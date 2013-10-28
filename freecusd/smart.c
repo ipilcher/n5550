ipilcher/n5550

commit c6fde681c86d497b26875de61fef82fcc2b96bda

Check return value of monitor sleep function.

freecusd/smart.c
@@ -117,9 +117,12 @@ static void *fcd_smart_fn(void *arg)
 		fcd_copy_buf_and_alerts(mon, buf, warn, 0, disk_alerts);
 
 continue_outer_loop:
-		;	/* C requires a statement after a label */
 
-	} while (fcd_lib_monitor_sleep(30) == 0);
+		i = fcd_lib_monitor_sleep(30);
+		if (i == -1)
+			fcd_disable_mon_cmd(mon, pipe_fds, NULL);
+
+	} while (i == 0);
 
 	fcd_proc_close_pipe(pipe_fds);
 	pthread_exit(NULL);

freecusd/cputemp.c
@@ -102,7 +102,11 @@ static void *fcd_cputemp_fn(void *arg)
 
 		fcd_copy_buf_and_alerts(mon, buf, warn, fail, NULL);
 
-	} while (fcd_lib_monitor_sleep(30) == 0);
+		ret = fcd_lib_monitor_sleep(30);
+		if (ret == -1)
+			fcd_cputemp_close_and_disable(fps, mon);
+
+	} while (ret == 0);
 
 	for (i = 0; i < 2; ++i) {
 		if (fclose(fps[i]) == EOF)

freecusd/hddtemp.c
@@ -167,7 +167,11 @@ static void *fcd_hddtemp_fn(void *arg)
 
 		fcd_copy_buf_and_alerts(mon, buf, warn, fail, disk_alerts);
 
-	} while (fcd_lib_monitor_sleep(30) == 0);
+		ret = fcd_lib_monitor_sleep(30);
+		if (ret == -1)
+			fcd_disable_mon_cmd(mon, pipe_fds, cmd_buf);
+
+	} while (ret == 0);
 
 	free(cmd_buf);
 	fcd_proc_close_pipe(pipe_fds);

freecusd/loadavg.c
@@ -81,7 +81,11 @@ static void *fcd_loadavg_fn(void *arg)
 
 		fcd_copy_buf_and_alerts(mon, buf, warn, fail, NULL);
 
-	} while (fcd_lib_monitor_sleep(30) == 0);
+		ret = fcd_lib_monitor_sleep(30);
+		if (ret == -1)
+			fcd_loadavg_close_and_disable(fp, mon);
+
+	} while (ret == 0);
 
 	if (fclose(fp) != 0)
 		FCD_PERROR("fclose");

freecusd/raid.c
@@ -1043,7 +1043,11 @@ static void *fcd_raid_fn(void *arg)
 
 		fcd_copy_buf_and_alerts(mon, buf, warn, fail, disks);
 
-	} while (fcd_lib_monitor_sleep(30) == 0);
+		ret = fcd_lib_monitor_sleep(30);
+		if (ret == -1)
+			fcd_raid_disable(mdstat_buf, fd, pipe_fds, mon);
+
+	} while (ret == 0);
 
 	fcd_raid_cleanup(mdstat_buf, fd, pipe_fds);
 	pthread_exit(NULL);

freecusd/sysfan.c
@@ -82,7 +82,11 @@ static void *fcd_sysfan_fn(void *arg)
 
 		fcd_copy_buf_and_alerts(mon, buf, warn, fail, NULL);
 
-	} while (fcd_lib_monitor_sleep(30) == 0);
+		ret = fcd_lib_monitor_sleep(30);
+		if (ret == -1)
+			fcd_sysfan_close_and_disable(fp, mon);
+
+	} while (ret == 0);
 
 	if (fclose(fp) != 0)
 		FCD_PERROR("fclose");

commit 0645b9d191dd9ed72c608c962d16849cd1c5da90

Fix SMART monitor bug introduced by c6fde681c86d497b26875de61fef82fcc2b96bda

freecusd/smart.c
@@ -82,7 +82,7 @@ static void *fcd_smart_fn(void *arg)
 			fcd_lib_disable_cmd_mon(mon, pipe_fds, NULL);
 
 		if (disks_changed == 0 && now - last < 1800)
-			continue;
+			goto continue_outer_loop;
 
 		last = now;
 
@@ -93,7 +93,7 @@ static void *fcd_smart_fn(void *arg)
 		{
 			if (disk_presence[i] == 0) {
 				memset(buf + i * 3, '-', 2);
-				continue;
+				continue;	/* inner loop */
 			}
 
 			status = fcd_smart_status(i, pipe_fds, mon);